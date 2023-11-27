c478633b39e2ff143a6f697feabb4e26a4f01e52
jvfd3 <joaovitorfd2000@gmail.com>
2023-11-27 15:06:23 -0300

    this place seems nicer

---
+++ Files/timetabling/src/DB/dataFromDB.js
@@ -0,0 +1,23 @@
+import options from "../code/temp/options";
+import { readData, updateData } from "../code/functions/CRUD_JSONBIN";
+
+let JB = options.JBVars;
+
+const DBData = {
+  disciplinas: await readData(JB.bins.infoDisciplinasCC),
+  professores: await readData(JB.bins.infoProfessores),
+  alunos: await readData(JB.bins.infoAlunos),
+  progressao: await readData(JB.bins.andamentoAlunos)
+};
+
+console.log("Am I exporting all data from DB?");
+
+// import { readData, updateData } from "../functions/CRUD_JSONBIN";
+//
+// let dataReceived = await readData();
+// console.log("valor que eu quero receber:", dataReceived);
+//
+// function MainPage() {
+// const [testDict, setTestDict] = useState(dataReceived);
+
+export { DBData };
---
+++ Files/timetabling/src/DB/dataFromJSON.js
@@ -0,0 +1,37 @@
+import andamentoAlunosData from "./JSON/dynamic/andamentoAlunos.json";
+import preferenciasProfessoresData from "./JSON/dynamic/preferenciasProfessores.json";
+import turmas_2022Data from "./JSON/dynamic/turmas_2022.json";
+import turmas_passadoData from "./JSON/dynamic/turmas_passado.json";
+import turmasData from "./JSON/dynamic/turmas.json";
+import RSalunosData from "./JSON/select/RSalunos.json";
+import RSdisciplinasData from "./JSON/select/RSdisciplinas.json";
+import RSprofessoresData from "./JSON/select/RSprofessores.json";
+import RSsalasData from "./JSON/select/RSsalas.json";
+import infoAlunosData from "./JSON/static/infoAlunos.json";
+import infoDisciplinasCCData from "./JSON/static/infoDisciplinasCC.json";
+import infoProfessoresData from "./JSON/static/infoProfessores.json";
+import infoSalasData from "./JSON/static/infoSalas.json";
+
+const allData = {
+    dynamic: {
+        andamentoAlunos: andamentoAlunosData,
+        preferenciasProfessores: preferenciasProfessoresData,
+        turmas_2022: turmas_2022Data,
+        turmas_passado: turmas_passadoData,
+        turmas: turmasData,
+    },
+    select: {
+        RSalunos: RSalunosData,
+        RSdisciplinas: RSdisciplinasData,
+        RSprofessores: RSprofessoresData,
+        RSsalas: RSsalasData,
+    },
+    static: {
+        infoAlunos: infoAlunosData,
+        infoDisciplinasCC: infoDisciplinasCCData,
+        infoProfessores: infoProfessoresData,
+        infoSalas: infoSalasData,
+    }
+}
+
+export {allData};
