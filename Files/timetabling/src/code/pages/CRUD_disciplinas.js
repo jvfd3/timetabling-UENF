9f332c0d78ee4352200fc30ed93f762637569d5f
jvfd3 <joaovitorfd2000@gmail.com>
2023-11-27 23:31:35 -0300

    the page seems kinda ok

--- Files/timetabling/src/code/pages/CRUD_disciplinas.js
+++ Files/timetabling/src/code/pages/CRUD_disciplinas.js
@@ -5,7 +5,7 @@
   - Select de Período: à esquerda do Select de código
   - Select de professores deve estar abaixo do Select de Códigos.
   - Select de requisitos deve estar abaixo do Select de professores.
-- Devo obter as informações das disciplinas do JSONBIN
+- [x] Devo obter as informações das disciplinas do JSONBIN
 - Transformar as informações para que cada nome de disciplina esteja na key "label" e o código na key "value"
 - devo converter a lista de codigos de requisitos para uma lista de dicionários com a estrutura {label: "nome", value: "codigo"}
   - essa lista deve ser definida como value do Select de requisitos
@@ -22,29 +22,79 @@ import "../CSS/CRUD_disciplinas.css";
 import "../CSS/defaultStyle.css";
 import options from "../temp/options";
 import CRUDPageSelection from "../components/PageSelect";
-import MySelectList from "../components/MySelectList";
-// import {
-//   getNomesDasDisciplinas,
-//   disciplinas_RS,
-// } from "../functions/getListaDisciplinas";
-// import { readData } from "../functions/CRUD_JSONBIN";
+import Select from "react-select";
+import { readData } from "../functions/CRUD_JSONBIN";
+import { disciplinaDBtoRS } from "../functions/convertingBetweenDBandRS";
+
+let disciplinasFromJB = await readData(options.JBVars.bins.infoDisciplinasCC);
+let disciplinas_RS = disciplinasFromJB.map(disciplinaDBtoRS);
+
+function updatingSelect(newValue, coletivo, setColetivo, individual, setIndividual) {
+  console.log("Updating Select");
+  console.log("individual: ", individual);
+
+  let myIndividual = { ...individual };
+  myIndividual["periodo"] = newValue.value;
+  setIndividual(myIndividual);
+
+  console.log("newValue: ", newValue);
+  console.log("individual: ", individual);
+}
 
 function DisciplinaCard() {
-  // const [disciplinas, setDisciplinas] = useState(RSdisciplinas);
-  // const [disciplina, setDisciplina] = useState(disciplinas[2]);
+  const [disciplinas, setDisciplinas] = useState(disciplinas_RS);
+  const [disciplina, setDisciplina] = useState(disciplinas[36]);
 
+  function getCorrectPeriodo(periodo) {
+    let correctPeriodo = options.expectedSemester.find(
+      (option) => option.value === periodo
+    );
+    return correctPeriodo;
+  }
+  console.log("Disciplina: ", disciplina);
   return (
     <div className="CRUD-subjects">
-      <MySelectList
-        isLabeled={true}
-        newPlaceHolder="Disciplina"
-        options={options.subjectCodeName}
+      <Select
+        placeholder={"Disciplina"}
+        value={disciplina}
+        options={disciplinas}
+        onChange={setDisciplina}
+        // formatOptionLabel={props.formatOptionLabel}
+        getOptionLabel={(option) => `${option.value}: ${option.label}`}
         isMulti={false}
+        className="SelectList-disciplinas"
       />
-      <MySelectList
-        newPlaceHolder="Período Esperado"
-        options={options.expectedSemester}
-      />
+      <p>
+        {disciplina.value}: {disciplina.label}
+      </p>
+      <div>
+        PERÍODO:
+        <Select
+          newPlaceHolder="Período Esperado"
+          value={getCorrectPeriodo(disciplina.periodo)}
+          onChange={(newValue) => {updatingSelect(newValue, disciplinas, setDisciplinas, disciplina, setDisciplina)}}
+          options={options.expectedSemester}
+        />
+      </div>
+      <div>
+        REQUISITOS:
+        <Select
+          placeholder={"REQUISITOS"}
+          options={disciplina.requisitos} // deveriam ser todas as disciplinas
+          value={disciplina.requisitos}
+          onChange={(option) => {
+            let myNewValue = { ...disciplina };
+            myNewValue["requisitos"] = option;
+            setDisciplina(myNewValue);
+          }}
+          className="SelectDisciplinas"
+          isMulti={true}
+          isClearable={true}
+          isSearchable={true}
+          getOptionLabel={(option) => `${option.value}: ${option.label}`}
+        />
+      </div>
+
       {/*
       <MySelectList
         isLabeled={true}
