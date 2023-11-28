f512442f8b52e035a0b9026ae815c2c0c133ed76
jvfd3 <joaovitorfd2000@gmail.com>
2023-11-27 23:29:18 -0300

    this is messy. I'll need to change later

--- Files/timetabling/src/code/functions/getListaDisciplinas.js
+++ Files/timetabling/src/code/functions/getListaDisciplinas.js
@@ -1,38 +1,44 @@
 import options from "../temp/options";
-import {readData} from "../functions/CRUD_JSONBIN";
+import { readData } from "../functions/CRUD_JSONBIN";
 
 // import {allLocalJsonData} from "../../DB/dataFromJSON";
 
 // let disciplinasInfoDB = allLocalJsonData.static.infoDisciplinasCC;
 
-
 let DBdisciplinas = await readData(options.JBVars.bins.infoDisciplinasCC);
 
 function getNomeDisciplina(codigoDisciplina) {
-    let disciplina = DBdisciplinas.find(
-      (disciplina) => disciplina.codigo === codigoDisciplina
-    );
-    return disciplina.nome;
-  }
-  
+  let disciplina = DBdisciplinas.find(
+    (disciplina) => disciplina.codigo === codigoDisciplina
+  );
+  return disciplina.nome;
+}
+
 function getNomesDasDisciplinas(listaDeCodigos) {
-let listaDeCodigosNomes = [];
-for (let i = 0; i < listaDeCodigos.length; i++) {
+  let listaDeCodigosNomes = [];
+  for (let i = 0; i < listaDeCodigos.length; i++) {
     let codigo = listaDeCodigos[i];
     let nome = getNomeDisciplina(codigo);
     listaDeCodigosNomes.push({ value: codigo, label: nome });
+  }
+  return listaDeCodigosNomes;
 }
-return listaDeCodigosNomes;
-}
+
+// function getNomeDisciplina(codigoDisciplina) {
+//   let disciplina = infoDisciplinasJsonData.find(
+//     (disciplina) => disciplina.codigo === codigoDisciplina
+//   );
+//   return disciplina.nome;
+// }
 
 function getCodigoNomeDisciplinas() {
   let disciplinas_RS = DBdisciplinas.map((disciplina) => ({
     value: disciplina.codigo,
     label: disciplina.nome,
-}));
-return disciplinas_RS;
+  }));
+  return disciplinas_RS;
 }
 
-const disciplinas_RS = getCodigoNomeDisciplinas();
+// const disciplinas_RS = getCodigoNomeDisciplinas();
 
-export {getNomesDasDisciplinas, getCodigoNomeDisciplinas}
+export { getNomesDasDisciplinas, getCodigoNomeDisciplinas };
