bc5ea2a699d1bd072a481ea6de2ad857875bc753
jvfd3 <joaovitorfd2000@gmail.com>
2023-11-27 23:29:40 -0300

    externalizing and organizing the updates of DB

--- Files/timetabling/src/code/functions/update_DB.js
+++ Files/timetabling/src/code/functions/update_DB.js
@@ -1,13 +1,36 @@
-import { allLocalJsonData } from "../../DB/dataFromJSON"
-import { options } from "../temp/options"
-import { updateData } from "./CRUD_JSONBIN"
+import { allLocalJsonData } from "../../DB/dataFromJSON";
+import options from "../temp/options";
+import { updateData } from "./CRUD_JSONBIN";
 
-console.log("ovô atualizar")
+console.log("ovô atualizar");
+
+function updateDB(bin) {
+  let data;
+  switch (bin) {
+    // PROFESORES
+    case options.JBVars.bins.infoProfessores:         data = allLocalJsonData.static.infoProfessores;          break;
+    // SALAS
+    case options.JBVars.bins.infoSalas:               data = allLocalJsonData.static.infoSalas;                break;
+    // DISCIPLINAS
+    case options.JBVars.bins.infoDisciplinasCC:       data = allLocalJsonData.static.infoDisciplinasCC;        break;
+    // ALUNOS
+    case options.JBVars.bins.infoAlunos:              data = allLocalJsonData.static.infoAlunos;               break;
+    // ANDAMENTO ALUNOS
+    case options.JBVars.bins.andamentoAlunos:         data = allLocalJsonData.dynamic.andamentoAlunos;         break;
+    // PREFERENCIA DE PROFESSORES
+    case options.JBVars.bins.preferenciasProfessores: data = allLocalJsonData.dynamic.preferenciasProfessores; break;
+    default: break;
+  }
+  updateData(data, bin);
+}
+
 // updateData(allLocalJsonData.static.infoProfessores, options.JBVars.bins.infoProfessores)
 // updateData(allLocalJsonData.static.infoSalas, options.JBVars.bins.infoSalas)
-// updateData(allLocalJsonData.static.infoDisciplinasCC, options.JBVars.bins.infoDisciplinasCC)
+// updateData(allLocalJsonData.static.infoDisciplinasCC, options.JBVars.bins.infoDisciplinasCC);
 // updateData(allLocalJsonData.static.infoAlunos, options.JBVars.bins.infoAlunos)
 // updateData(allLocalJsonData.dynamic.andamentoAlunos, options.JBVars.bins.andamentoAlunos)
-updateData(allLocalJsonData.dynamic.preferenciasProfessores, options.JBVars.bins.preferenciasProfessores)
+// updateData(allLocalJsonData.dynamic.preferenciasProfessores, options.JBVars.bins.preferenciasProfessores)
 
-console.log("atualizô?")
+console.log("atualizô?");
+
+export {updateDB};
