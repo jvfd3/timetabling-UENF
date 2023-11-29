6a309e769358baae1d71e1a771fa5d8597339fdb
jvfd3 <joaovitorfd2000@gmail.com>
2023-11-29 15:55:55 -0300

    changed data, and now the salas' data is being read

--- Files/timetabling/src/code/pages/CRUD_rooms.js
+++ Files/timetabling/src/code/pages/CRUD_rooms.js
@@ -2,36 +2,64 @@ import "../CSS/CRUD_rooms.css";
 import "../CSS/defaultStyle.css";
 import assets from "../../assets/imagesImport";
 import options from "../temp/options";
-import MySelectList from "../components/MySelectList";
 import CRUDPageSelection from "../components/PageSelect";
+import { allLocalJsonData } from "../../DB/dataFromJSON";
+import react, { useState } from "react";
+import Select from "react-select";
 
 function Salas() {
+  function getSalasData() {
+    let salasFromJson = allLocalJsonData.static.infoSalas;
+    // console.log(salasList)
+    return salasFromJson;
+  }
+
+  let salasFromJson = getSalasData();
+
+  const [salas, setSalas] = useState(salasFromJson);
+  const [sala, setSala] = useState(salas[0]);
+
+  function VisualizacaoSala({ currentSala }) {
+    return (
+      <div>
+        <table>
+          <tr>
+            <th>Bloco</th>
+            <td>{`${currentSala.bloco} (${currentSala.descricaoBloco})`}</td>
+          </tr>
+          <tr>
+            <th>Código</th>
+            <td>{currentSala.codigo}</td>
+          </tr>
+          <tr>
+            <th>Capacidade</th>
+            <td>{currentSala.capacidade}</td>
+          </tr>
+        </table>
+        <img className="CRUD-room-placeholderimg" src={assets.room} alt="" />
+      </div>
+    );
+  }
+
   return (
     <div className="CRUD-outro">
-      <div className="CRUD-properties">
-        <MySelectList
-          newPlaceHolder="Sala"
-          options={options.roomCodes}
-          isMulti={false}
-          isLabeled={true}
+      <div
+        style={{ backgroundColor: "#996633", padding: 20 }}
+        className="CRUD-properties"
+      >
+        <Select
+          className="CRUD-room-select"
+          options={salas}
+          value={sala}
+          onChange={setSala}
+          getOptionValue={(option) => option.blocoSala}
+          getOptionLabel={(option) => option.capacidade}
+          formatOptionLabel={(sala) =>
+            `(${sala.capacidade}) ${sala.bloco}-${sala.codigo}`
+          }
         />
-        <input
-          required={true}
-          type="number"
-          placeholder="Quantidade de cadeiras"
-        />
-        <input
-          required={true}
-          type="number"
-          placeholder="Quantidade de computadores"
-        />
-        <MySelectList
-          newPlaceHolder="Propriedades da sala"
-          options={options.roomProps}
-          isMulti={true}
-        />
+        <VisualizacaoSala currentSala={sala} />
       </div>
-      {/* <img className="CRUD-room-placeholderimg" src={assets.room} alt="" /> */}
     </div>
   );
 }
