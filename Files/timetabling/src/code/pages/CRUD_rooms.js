90bb9e6d206f09a0460b1c2c19885b09473361aa
jvfd3 <joaovitorfd2000@gmail.com>
2023-11-29 16:52:30 -0300

    Now Salas can see which classes occur there

--- Files/timetabling/src/code/pages/CRUD_rooms.js
+++ Files/timetabling/src/code/pages/CRUD_rooms.js
@@ -7,22 +7,76 @@ import { allLocalJsonData } from "../../DB/dataFromJSON";
 import react, { useState } from "react";
 import Select from "react-select";
 
-function Salas() {
-  function getSalasData() {
-    let salasFromJson = allLocalJsonData.static.infoSalas;
-    // console.log(salasList)
-    return salasFromJson;
+function TurmasNaSala({ blocoSala }) {
+  function getTurmas() {
+    let codigoDaSalaDoBloco = blocoSala;
+    let turmasTeste = allLocalJsonData.dynamic.turmasTeste;
+    // console.log(turmasTeste);
+    let horariosNestaSala = [];
+    for (const chaveTurma in turmasTeste) {
+      let turma = turmasTeste[chaveTurma];
+      for (const chaveHorario in turma.horarios) {
+        let horario = turma.horarios[chaveHorario];
+        let salaDoHorario = horario.sala;
+        // console.log(salaDoHorario);
+        // console.log(blocoSala);
+        if (blocoSala === salaDoHorario) {
+          let newTurma = { ...turmasTeste[chaveTurma] };
+          newTurma.horarios = { ...horario };
+          horariosNestaSala.push(newTurma);
+        }
+      }
+    }
+    return horariosNestaSala;
   }
 
-  let salasFromJson = getSalasData();
+  let turmasNestaSala = getTurmas();
+  console.log(turmasNestaSala);
+  return (
+    <div>
+      <h4>Informações de Turmas nesta sala</h4>
+      <table>
+        <thead>
+          <tr>
+            <th>Ano.Semestre</th>
+            <th>Disciplina</th>
+            <th>Professor</th>
+            <th>Hora Início</th>
+            <th>Duração</th>
+          </tr>
+        </thead>
+        <tbody>
+          {turmasNestaSala.map((turma, i) => {
+            return (
+              <tr key={i}>
+                <td>
+                  {turma.ano}.{turma.semestre}
+                </td>
+                <td>
+                  {turma.disciplina.codigo} {turma.disciplina.nome}
+                </td>
+                <td>{turma.professor}</td>
+                <td>{turma.horarios.horaInicio}</td>
+                <td>{turma.horarios.duracao}</td>
+                <td>
+                  <button key={i}>Remover</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
-  const [salas, setSalas] = useState(salasFromJson);
-  const [sala, setSala] = useState(salas[0]);
-
-  function VisualizacaoSala({ currentSala }) {
-    return (
-      <div>
-        <table>
+function VisualizacaoSala(props) {
+  let { currentSala } = props;
+  return (
+    <div>
+      <h4>Informações da Sala</h4>
+      <table>
+        <thead>
           <tr>
             <th>Bloco</th>
             <td>{`${currentSala.bloco} (${currentSala.descricaoBloco})`}</td>
@@ -35,10 +89,24 @@ function Salas() {
             <th>Capacidade</th>
             <td>{currentSala.capacidade}</td>
           </tr>
-        </table>
-        <img className="CRUD-room-placeholderimg" src={assets.room} alt="" />
-      </div>
-    );
+        </thead>
+      </table>
+      <TurmasNaSala blocoSala={currentSala.blocoSala} />
+      <img className="CRUD-room-placeholderimg" src={assets.room} alt="" />
+    </div>
+  );
+}
+
+function Salas() {
+  let salasFromJson = getSalasData();
+
+  const [salas, setSalas] = useState(salasFromJson);
+  const [sala, setSala] = useState(salas[0]);
+
+  function getSalasData() {
+    let salasFromJson = allLocalJsonData.static.infoSalas;
+    // console.log(salasList)
+    return salasFromJson;
   }
 
   return (
