22f867e9cd6fb294d7ffb986e15177607b654c4a
jvfd3 <joaovitorfd2000@gmail.com>
2023-11-29 14:32:03 -0300

    going for rooms

--- Files/timetabling/src/code/pages/CRUD_rooms.js
+++ Files/timetabling/src/code/pages/CRUD_rooms.js
@@ -5,37 +5,43 @@ import options from "../temp/options";
 import MySelectList from "../components/MySelectList";
 import CRUDPageSelection from "../components/PageSelect";
 
+function Salas() {
+  return (
+    <div className="CRUD-outro">
+      <div className="CRUD-properties">
+        <MySelectList
+          newPlaceHolder="Sala"
+          options={options.roomCodes}
+          isMulti={false}
+          isLabeled={true}
+        />
+        <input
+          required={true}
+          type="number"
+          placeholder="Quantidade de cadeiras"
+        />
+        <input
+          required={true}
+          type="number"
+          placeholder="Quantidade de computadores"
+        />
+        <MySelectList
+          newPlaceHolder="Propriedades da sala"
+          options={options.roomProps}
+          isMulti={true}
+        />
+      </div>
+      {/* <img className="CRUD-room-placeholderimg" src={assets.room} alt="" /> */}
+    </div>
+  );
+}
+
 function CRUDrooms() {
   return (
     <div className="background">
       <div className="CRUD-contain-components">
         <CRUDPageSelection defaultValue={options.CRUD.crud_salas} />
-        <div className="CRUD-outro">
-          <div className="CRUD-properties">
-            <MySelectList
-              newPlaceHolder="Sala"
-              options={options.roomCodes}
-              isMulti={false}
-              isLabeled={true}
-            />
-            <input
-              required={true}
-              type="number"
-              placeholder="Quantidade de cadeiras"
-            />
-            <input
-              required={true}
-              type="number"
-              placeholder="Quantidade de computadores"
-            />
-            <MySelectList
-              newPlaceHolder="Propriedades da sala"
-              options={options.roomProps}
-              isMulti={true}
-            />
-          </div>
-          <img className="CRUD-room-placeholderimg" src={assets.room} alt="" />
-        </div>
+        <Salas />
       </div>
     </div>
   );
