2479af36d1efa256fd738329c99d172f99127a52
jvfd3 <joaovitorfd2000@gmail.com>
2023-11-28 21:24:41 -0300

    trying to fix Períodos de horários

--- Files/timetabling/src/code/pages/CRUD_class.js
+++ Files/timetabling/src/code/pages/CRUD_class.js
@@ -3,35 +3,12 @@ import "../CSS/defaultStyle.css";
 import "../CSS/participantList.css";
 import options from "../temp/options";
 import assets from "../../assets/imagesImport";
-import MySelectList from "../components/MySelectList";
 import CRUDPageSelection from "../components/PageSelect";
-
-const Timeperiod = () => {
-  return (
-    <div className="CRUD-timeperiod-properties">
-      <div className="CRUD-timeperiod-property">
-        Hora de início
-        <input placeholder="9:00"></input>
-      </div>
-      <div className="CRUD-timeperiod-property">
-        Tempo de duração
-        <input placeholder="2:00"></input>
-      </div>
-      <div className="CRUD-timeperiod-property">
-        Dia
-        <MySelectList options={options.days} newPlaceHolder={"Dia da semana"} />
-      </div>
-      <div className="CRUD-timeperiod-property-room">
-        Sala
-        <MySelectList
-          isLabeled={true}
-          options={options.roomCodes}
-          newPlaceHolder={"Sala"}
-        />
-      </div>
-    </div>
-  );
-};
+import React, { useState, useEffect } from "react";
+import Select from "react-select";
+import { allLocalJsonData } from "../../DB/dataFromJSON";
+import AsyncSelect from "react-select/async";
+import { readData } from "../functions/CRUD_JSONBIN";
 
 const CRUDParticipants = () => {
   return (
@@ -51,9 +28,9 @@ const CRUDParticipants = () => {
         <ul>
           {options.students.map((student) => (
             <li key={student.value}>
-              <t className="participants-participant">
+              <p className="participants-participant">
                 {student.value}: {student.label}
-              </t>
+              </p>
             </li>
           ))}
         </ul>
@@ -62,45 +39,186 @@ const CRUDParticipants = () => {
   );
 };
 
+const Timeperiod = (props) => {
+  const { horarioTP, sethorariosTP } = props;
+  console.log("Timeperiod>horarioTP", horarioTP);
+  const [horario, setHorario] = useState({ ...horarioTP });
+
+  const [horaInicio, setHoraInicio] = useState(options.hours.find((hour) => hour.value === horario.horaInicio));
+  // const [duracao, setDuracao] = useState(convertToValueLabel(horario.duracao));
+  // const [dia, setDia] = useState(convertToValueLabel(horario.dia));
+  // const [sala, setSala] = useState(convertToValueLabel(horario.dia));
+  // console.log(horaInicio);
+  return (
+    <div className="CRUD-timeperiod-properties">
+      <div className="CRUD-timeperiod-property">
+        Hora de início
+        <Select
+          placeholder="Hora início"
+          options={options.hours}
+          value={horaInicio}
+          onChange={setHoraInicio}
+          formatOptionLabel={({ value, label }) => `${value} (${label})`}
+        />
+      </div>
+      {/*       <div className="CRUD-timeperiod-property">
+        Tempo de duração
+        <Select placeholder="Duração" options={options.durations} />
+      </div>
+      <div className="CRUD-timeperiod-property">
+        Dia
+        <Select placeholder="Dia" options={options.days} />
+      </div>
+      <div className="CRUD-timeperiod-property">
+        Sala
+        <Select placeholder="Sala" options={options.rooms} />
+      </div> */}
+    </div>
+  );
+};
+
+function TimePeriods(props) {
+  const { horariosTPs, setHorariosTPs } = props;
+  console.log("TimePeriods>horariosTPs", horariosTPs[0]);
+  return (
+    <div className="CRUD-Class-properties-timeperiods">
+      <p className="CRUD-Class-properties-timeperiods-title">
+        Períodos de horários
+      </p>
+      <Timeperiod horarioTP={horariosTPs[0]} sethorariosTP={setHorariosTPs} />
+      <Timeperiod horarioTP={horariosTPs[1]} sethorariosTP={setHorariosTPs} />
+      <img
+        className="CRUD-Class-properties-NewTimePeriod-icon"
+        src={assets.icons.add}
+        alt=""
+      />
+    </div>
+  );
+}
+
+function convertToValueLabel(newValue) {
+  return { value: newValue, label: newValue };
+}
+
+let DBdisciplinas = await readData(options.JBVars.bins.infoDisciplinasCC);
+
+function SelectDisciplina(props) {
+  const { disciplinaSD, setDisciplinaSD } = props;
+  const [disciplinas, setDisciplinas] = useState(DBdisciplinas);
+  return (
+    <div>
+      <Select
+        isMulti={false}
+        value={disciplinaSD}
+        isClearable={false}
+        isSearchable={true}
+        options={disciplinas}
+        onChange={setDisciplinaSD}
+        placeholder="Disciplinas"
+        className="SelectDisciplinas"
+        getOptionLabel={(option) => option.nome}
+        getOptionValue={(option) => option.codigo}
+        formatOptionLabel={({ codigo, nome }) => `${codigo}: ${nome}`}
+      />
+    </div>
+  );
+}
+
+function SelecaoDeTurma() {
+  /* 2. Select de Ano
+   1. State de ano
+   2. Filtra todas as turmas para apenas mostrar as turmas daquele ano */
+
+  const [turmas, setTurmas] = useState(allLocalJsonData.dynamic.turmasTeste);
+  const [turma, setTurma] = useState(turmas[0]);
+  const [ano, setAno] = useState(convertToValueLabel(turma.ano));
+  const [semestre, setSemestre] = useState(convertToValueLabel(turma.semestre));
+  const [professor, setProfessor] = useState(
+    convertToValueLabel(turma.professor)
+  );
+  const [disciplina, setDisciplina] = useState({
+    codigo: turma.disciplina.codigo,
+    nome: turma.disciplina.nome,
+  });
+
+  const [horarios, setHorarios] = useState(turma.horarios);
+
+  function updateTurma(newTurmaValue) {
+    setTurma(newTurmaValue);
+    setAno(convertToValueLabel(newTurmaValue.ano));
+    setSemestre(convertToValueLabel(newTurmaValue.semestre));
+    setDisciplina({
+      codigo: newTurmaValue.disciplina.codigo,
+      nome: newTurmaValue.disciplina.nome,
+    });
+    setProfessor(convertToValueLabel(newTurmaValue.professor));
+    setHorarios(newTurmaValue.horarios);
+    console.log("SelecaoTurma>updateTurma>turma.horarios", turma.horarios[0]);
+    console.log("SelecaoTurma>updateTurma>horarios", horarios[0]);
+    console.log(
+      "SelecaoTurma>updateTurma>newTurmaValue.horarios",
+      newTurmaValue.horarios[0]
+    );
+  }
+
+  // console.log(turma);
+  return (
+    <div style={{ color: "#000000" }}>
+      <div>
+        <Select
+          placeholder="Turma"
+          options={turmas}
+          value={turma}
+          onChange={(newTurma) => {
+            updateTurma(newTurma);
+          }}
+          getOptionLabel={(turma) => turma.disciplina.codigo}
+          getOptionValue={(turma) => turma.professor}
+          formatOptionLabel={(turma, { context }) => `${turma.ano}.${turma.semestre} - ${turma.disciplina.codigo} - ${turma.professor}`}
+        />
+        <div style={{ display: "flex", color: "#000000" }}>
+          <Select
+            placeholder="Ano"
+            options={options.years}
+            value={ano}
+            onChange={(newValue) => setAno(newValue)}
+          />
+          <Select
+            placeholder="Semestre"
+            options={options.semesters}
+            value={semestre}
+            onChange={(newValue) => setSemestre(newValue)}
+          />
+        </div>
+        <SelectDisciplina
+          disciplinaSD={disciplina}
+          setDisciplinaSd={setDisciplina}
+        />
+        {/* <Select placeholder="Disciplinas" /> */}
+        <Select
+          placeholder="Professor"
+          value={professor}
+          onChange={(newValue) => setProfessor(newValue)}
+          options={options.professors}
+        />
+      </div>
+      <TimePeriods horariosTPs={horarios} setHorariosTPs={setHorarios} />
+    </div>
+  );
+}
+
 function CRUDclass() {
   return (
     <div className="background">
-      <CRUDPageSelection defaultValue={options.CRUD.crud_turmas} />
+      {/* <CRUDPageSelection defaultValue={options.CRUD.crud_turmas} /> */}
       <div className="CRUD-lateral">
         <div className="CRUD-contain-components">
           <div className="CRUD-Class-properties">
-            <div className="CRUD-Class-properties-YearSemester">
-              <MySelectList newPlaceHolder={"Ano"} options={options.years} />
-              <MySelectList
-                newPlaceHolder={"Semestre"}
-                options={options.semesters}
-              />
-            </div>
-            <MySelectList
-              isLabeled={true}
-              newPlaceHolder={"Código: Nome da disciplina"}
-              options={options.subjectCodeName}
-            />
-            <MySelectList
-              newPlaceHolder={"Docente"}
-              options={options.professors}
-            />
-            <div className="CRUD-Class-properties-timeperiods">
-              <text className="CRUD-Class-properties-timeperiods-title">
-                Períodos de horários
-              </text>
-              <Timeperiod />
-              <Timeperiod />
-              <img
-                className="CRUD-Class-properties-NewTimePeriod-icon"
-                src={assets.icons.add}
-                alt=""
-              />
-            </div>
+            <SelecaoDeTurma />
           </div>
-          <img className="placeholder-image" src={assets.class} alt="Logo" />
+          {/* <img className="placeholder-image" src={assets.class} alt="Logo" /> */}
         </div>
-        <CRUDParticipants />
+        {/* <CRUDParticipants /> */}
       </div>
     </div>
   );
