5514ac5c1ea41e196750c7fe54f1bf54563bc8a8
jvfd3 <joaovitorfd2000@gmail.com>
2023-11-29 21:26:43 -0300

    removing the empty studentCard CSS

--- Files/timetabling/src/code/pages/CRUD_students.js
+++ Files/timetabling/src/code/pages/CRUD_students.js
@@ -1,6 +1,5 @@
 import "../CSS/CRUD_students.css";
 import "../CSS/defaultStyle.css";
-import "../components/studentCard/studentCard.css";
 import React, { useState } from "react";
 import options from "../temp/options";
 import CRUDPageSelection from "../components/PageSelect";
