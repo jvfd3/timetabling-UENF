3dacb48263db83267520533621aaa114e781b8c3
jvfd3 <joaovitorfd2000@gmail.com>
2023-12-01 16:00:24 -0300

    Change by scrolling

--- Files/timetabling/src/code/components/PageSelect.js
+++ Files/timetabling/src/code/components/PageSelect.js
@@ -19,8 +19,25 @@ const CRUDPageSelection = (props) => {
     (option) => option.label !== "Not Found" && option.label !== "Main CRUD"
   );
 
+  function chagePageByScrolling(event) {
+    let diretion = event.deltaY > 0 ? "down" : "up";
+    // console.log("Scrolling ", diretion);
+    let index = filteredOptions.findIndex(
+      (option) => option.value === props.defaultValue.value
+    );
+    // console.log("Came from Index: ", index);
+    index += diretion === "up" ? -1 : 1;
+    // console.log("Trying to go to Index: ", index);
+    index = index < 0 ? filteredOptions.length - 1 : index;
+    index = index >= filteredOptions.length ? 0 : index;
+    // console.log("Actually going to Index: ", index);
+    let newOption = filteredOptions[index];
+    // console.log("new Page: ", newOption);
+    handleChange(newOption);
+  }
+
   return (
-    <div className="CRUD-page-selection">
+    <div className="CRUD-page-selection" onWheel={chagePageByScrolling}>
       <Select
         placeholder={"Selecionar CRUD"}
         options={filteredOptions} // Use as opções filtradas aqui
