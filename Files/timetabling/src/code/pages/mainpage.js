f80d2b8d498064858a712c248bd4dd120024b6d8
jvfd3 <joaovitorfd2000@gmail.com>
2023-12-02 16:48:49 -0300

    Informando os atalhos

--- Files/timetabling/src/code/pages/mainpage.js
+++ Files/timetabling/src/code/pages/mainpage.js
@@ -8,6 +8,22 @@ function Main() {
     <div className="CRUDContainComponents">
       <h1>Welcome to my monograph's website!</h1>
       <p>This is the main page.</p>
+      <h4>Atalhos</h4>
+      <ul>
+        <li>
+          <strong>Mudar páginas</strong>: passar o mouse por cima do menu no
+          canto superior esquerdo da tela e usar o scroll do mouse
+        </li>
+        <li>
+          <strong>Selecionar um item</strong>: passar o mouse por cima da
+          seleção de itens e usar o scroll do mouse
+        </li>
+        <li>
+          <strong>Obs.:</strong>: quando a página é longa e tem scroll,
+          pressione a letra "s" para impedir a rolagem da página e poder usar o
+          scroll do mouse para selecionar itens.
+        </li>
+      </ul>
     </div>
   );
 }
