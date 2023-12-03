6d1992377f2f67eca2eed4a2cdcb1d2f12a8b338
jvfd3 <joaovitorfd2000@gmail.com>
2023-12-03 14:56:03 -0300

    Novas funções auxiliares

--- Files/timetabling/src/code/functions/conflicts/auxiliarConflictsFunctions.js
+++ Files/timetabling/src/code/functions/conflicts/auxiliarConflictsFunctions.js
@@ -1,12 +1,37 @@
-// import options from "../../temp/options";
-// import { allLocalJsonData } from "../../../DB/dataFromJSON";
+import options from "../../temp/options";
+import { allLocalJsonData } from "../../../DB/dataFromJSON";
 
-function getTurmasPorAnoESemestre(turmas, ano, semestre) {
+function testingTurmas2022_1() {
+  let anos = options.constantValues.years;
+  let ano = anos[8].value;
+
+  let semestres = options.constantValues.semesters;
+  let semestre = semestres[0].value;
+
+  let turmas = allLocalJsonData.tests.turmasTesteAlunos;
+  let turmas2022_1 = getTurmasPorAnoESemestre(turmas, ano, semestre);
+  return turmas2022_1;
+}
+
+function splitTurmas(turmas) {
+  let newSplittedTurmas = [];
+  turmas.forEach((turma) => {
+    turma.horarios.forEach((horario) => {
+      let newTurma = { ...turma, ...horario };
+      delete newTurma.horarios;
+      newSplittedTurmas.push(newTurma);
+    });
+  });
+  return newSplittedTurmas;
+}
+
+function getTurmasPorAnoESemestre(turmas, ano, semestre, splitted = false) {
   /*
     Essa função retorna um array de turmas que acontecem no ano e semestre passados como parâmetro.
     Ela também expande os horários de cada turma em um objeto separado, para facilitar a análise de conflitos.
   */
-  let turmasDesseSemestre = [];
+
+  /* // How it was done before
   turmas.forEach((turma) => {
     if (turma.ano === ano && turma.semestre === semestre) {
       turma.horarios.forEach((horario) => {
@@ -16,7 +41,35 @@ function getTurmasPorAnoESemestre(turmas, ano, semestre) {
       });
     }
   });
-  return turmasDesseSemestre;
+  */
+  let turmasDoAno = searchListForKeyWithValue(turmas, "ano", ano);
+  let turmasDoSemestre = searchListForKeyWithValue(
+    turmasDoAno,
+    "semestre",
+    semestre
+  );
+
+  if (!splitted) {
+    return turmasDoSemestre;
+  }
+  let splittedTurmas = splitTurmas(turmasDoSemestre);
+
+  return splittedTurmas;
+}
+
+function searchListForKeyWithValue(lista, chaveDaLista, valorAComparar) {
+  /*
+  O primeiro valor é uma lista de objetos
+  O segundo valor é uma string que representa uma chave de cada um dos objetos da lista
+  O terceiro valor é o valor que a chave deve ter para que o objeto seja retornado
+  */
+  let listaFiltrada = [];
+  lista.forEach((elemento) => {
+    if (elemento[chaveDaLista] === valorAComparar) {
+      listaFiltrada.push(elemento);
+    }
+  });
+  return listaFiltrada;
 }
 
 function getNumeroDeConflitos(conflitos) {
@@ -25,4 +78,10 @@ function getNumeroDeConflitos(conflitos) {
   return numeroDeConflitos;
 }
 
-export { getTurmasPorAnoESemestre, getNumeroDeConflitos };
+export {
+  testingTurmas2022_1,
+  splitTurmas,
+  getTurmasPorAnoESemestre,
+  searchListForKeyWithValue,
+  getNumeroDeConflitos,
+};
