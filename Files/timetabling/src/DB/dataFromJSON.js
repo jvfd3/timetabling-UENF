31a6fa976a929f14a4ea1f451798d17cdf313f9b
jvfd3 <joaovitorfd2000@gmail.com>
2023-12-03 21:25:14 -0300

    patternizing JSON export

--- Files/timetabling/src/DB/dataFromJSON.js
+++ Files/timetabling/src/DB/dataFromJSON.js
@@ -1,20 +1,27 @@
+/* Dynamic */
 import andamentoAlunosData from "./JSON/dynamic/andamentoAlunos.json";
 import preferenciasProfessoresData from "./JSON/dynamic/preferenciasProfessores.json";
 import turmasData from "./JSON/dynamic/turmas.json";
+/* Select */
 import RSalunosData from "./JSON/select/RSalunos.json";
 import RSdisciplinasData from "./JSON/select/RSdisciplinas.json";
 import RSprofessoresData from "./JSON/select/RSprofessores.json";
 import RSsalasData from "./JSON/select/RSsalas.json";
+/* Static */
 import infoAlunosData from "./JSON/static/infoAlunos.json";
 import infoDisciplinasCCData from "./JSON/static/infoDisciplinasCC.json";
 import infoProfessoresData from "./JSON/static/infoProfessores.json";
 import infoSalasData from "./JSON/static/infoSalas.json";
+/* Templates */
 import templatesData from "./JSON/templates/templates.json";
+/* Tests */
+import alocacaoDuplaAlunosData from "./JSON/tests/alocacaoDuplaAlunos.json";
+import alocacaoDuplaProfessorData from "./JSON/tests/alocacaoDuplaProfessor.json";
+import alocacaoDuplaSalaData from "./JSON/tests/alocacaoDuplaSala.json";
+import capacidadeSalasData from "./JSON/tests/capacidadeSalas.json";
+import demandaAndamentoAlunosData from "./JSON/tests/demandaAndamentoAlunos.json";
+import demandaTurmasData from "./JSON/tests/demandaTurmas.json";
 import turmasTesteData from "./JSON/tests/turmasTeste.json";
-import turmaTesteAlunosData from "./JSON/tests/turmasTesteAlunos.json";
-import turmasTesteConflitosProfessorData from "./JSON/tests/turmasTesteConflitosProfessor.json";
-import turmasTesteConflitosSalaData from "./JSON/tests/turmasTesteConflitosSala";
-
 const allLocalJsonData = {
   dynamic: {
     andamentoAlunos: andamentoAlunosData,
@@ -34,9 +41,12 @@ const allLocalJsonData = {
     infoSalas: infoSalasData,
   },
   tests: {
-    turmasTesteConflitosProfessor: turmasTesteConflitosProfessorData,
-    turmasTesteConflitosSala: turmasTesteConflitosSalaData,
-    turmasTesteAlunos: turmaTesteAlunosData,
+    alocacaoDuplaAlunos: alocacaoDuplaAlunosData,
+    alocacaoDuplaProfessor: alocacaoDuplaProfessorData,
+    alocacaoDuplaSala: alocacaoDuplaSalaData,
+    capacidadeSalas: capacidadeSalasData,
+    demandaAndamentoAlunos: demandaAndamentoAlunosData,
+    demandaTurmas: demandaTurmasData,
     turmasTeste: turmasTesteData,
   },
   templates: templatesData,
