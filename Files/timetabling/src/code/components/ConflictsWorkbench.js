1a600f0b727480d88f53eed05120785b143c7b94
jvfd3 <joaovitorfd2000@gmail.com>
2023-12-03 21:17:21 -0300

    limpando a área de trabalho

--- Files/timetabling/src/code/components/ConflictsWorkbench.js
+++ Files/timetabling/src/code/components/ConflictsWorkbench.js
@@ -11,123 +11,56 @@ import {
   getNumeroDeConflitos,
 } from "../functions/conflicts/auxiliarConflictsFunctions";
 
-/*  3. Um aluno com duas aulas ao mesmo tempo
+/* #### Capacidade da sala
+
 1. Padrão
    1. Selecionar um ano
    2. Selecionar um semestre
    3. Lista de turmas
    4. Filtrar as turmas do ano e semestre selecionados
 2. Aquisição dos dados
-   1. Lista de alunos
-   2. Selecionar um aluno
-      1. Criar uma lista com as matrículas de todos alunos
-      2. Selecionar uma das matrículas
-3. Filtrar as turmas referentes à matrícula
-   1. Atualmente tem duas formas:
-      1. tem a lista de disciplinas que o aluno tá cursando
-      2. E também tem a lista de alunos que estão na turma
-   2. Usarei a primeira
-   3. Retorna a lista de códigos de disciplinas que o aluno deseja cursar
-4. Preencher tabela de ocupação
-   1. Para cada disciplina que o aluno deseja cursar
-      1. Procurar se há turma disponível para essa disciplina
-      2. Se não houver, adicionar à lista `Demandado mas não ofertado`
-      3. Se houver, obter dia, início e duração
-         1. Adicionar à matriz de ocupação
-5. Checar o conflito
-   1. Para cada um dos dias da matriz de ocupação
-      1. para cada um dos horários
-         1. Se a lista for maior de 1,
-            1. Adiciona a uma lista o conjunto de horários que estão em conflito
-6. Mostrar os conflitos
-   1. console.log(lista de conflitos)
-
+   1. Lista de informações das salas
+      1. Selecionar uma sala
+   2. Selecionar todas as turmas que têm aula nessa sala (splitted)
+   3. Para cada turma
+      1. Agrupar as turmas por disciplina
+         1. Calcular as demandas dos alunos para essas disciplinas
+   4. Com isso finalizamos com a seguinte estrutura
+      1. `SalaEscolhida`
+      2. `turmasDaSala`
+      3. `codigoDisciplina`
+         1. `listaDeAlunos`
+         2. `listaDeTurmas`
+         3. `numeroDeAlunos`
+         4. `numeroDeTurmas`
+3. Checar o conflito
+   1. Para cada turma da sala
+      1. Se `codigoDisciplina.numeroDeTurmas` == 1:
+         1. Se `codigoDisciplina.numeroDeAlunos` > `SalaEscolhida.capacidade`:
+            1. Adiciona em uma lista o conjunto de horários que estão em conflito
+      2. Se `codigoDisciplina.numeroDeTurmas` != 1:
+         1. Se `codigoDisciplina.numeroDeTurmas` > 1:
+            1. Para cada turma em `codigoDisciplina.listaDeTurmas`
+               1. obter a sala com menor capacidade dentre os horários
+               2. somar as menores capacidades
+            2. Se a soma for menor que `codigoDisciplina.numeroDeAlunos`:
+               1. Adiciona em uma lista o conjunto de horários que estão em conflito
+4. Mostrar os conflitos
 */
 
-function ConflictsWorkbench() {
-  let turmas = testingTurmas2022_1();
+function CalculoDemandasAtendidas() {
+  let turmas = allLocalJsonData.tests.demandaTurmas;
+  turmas = testingTurmas2022_1(turmas);
 
-  console.log(turmas);
-
-  // let alunos = allLocalJsonData.dynamic.andamentoAlunos;
-  // let matriculasDeAlunos = getMatriculasAlunos(alunos);
-  // let matricula = matriculasDeAlunos[93];
-  //
-  // let disciplinasDoAluno = alunos[matricula].cursando;
-  // let turmasDoAluno = getTurmasDoAluno(turmasDesseSemestre, disciplinasDoAluno);
-  // let tabelaDeOcupacao = getTabelaDeOcupacao(turmasDoAluno);
-  // let conflitosEncontrados = getConflitos(tabelaDeOcupacao);
-  // let numeroDeConflitos = getNumeroDeConflitos(conflitosEncontrados);
-  //
-  // console.log("Checando conflitos de:", ano, semestre, matricula);
-  // console.log("Turmas desse semestre: ", turmasDesseSemestre);
-  // console.log("disciplinas do aluno: ", disciplinasDoAluno)
-  // console.log("turmas do aluno", turmasDoAluno)
-  // console.log("Procurando conflitos de:", ano, semestre, sala);
-  // console.log("turmas do sala: ", turmasDoAluno);
-  // console.log("tabela de ocupação: ", tabelaDeOcupacao);
-  // console.log("conflitos encontrados: ", conflitosEncontrados);
-  // console.log("numero de conflitos: ", numeroDeConflitos);
-
-  function getMatriculasAlunos(listaDeAlunos) {
-    return Object.keys(listaDeAlunos);
-  }
-
-  function getTurmasDoAluno(turmasDesseSemestre, disciplinasDoAluno) {
-    let turmasDoAluno = [];
-    turmasDesseSemestre.forEach((turma) => {
-      if (disciplinasDoAluno.includes(turma.disciplina.codigo)) {
-        turmasDoAluno.push(turma);
-      }
-    });
-    return turmasDoAluno;
-  }
-
-  function getTabelaDeOcupacao(turmas) {
-    let tabelaDeOcupacao = {};
-    turmas.forEach((turma) => {
-      /* Mudei aqui porque na função de cima, eu repeti as informações da sala para que cada horário fosse separado, porque não são todos os horários que estarão na mesma turma */
-      let horaInicio = parseInt(turma.horaInicio);
-      let duracao = parseInt(turma.duracao);
-      for (let i = 0; i < duracao; i++) {
-        let horarioAtual = horaInicio + i;
-        if (
-          tabelaDeOcupacao[turma.dia] &&
-          tabelaDeOcupacao[turma.dia][horarioAtual]
-        ) {
-          tabelaDeOcupacao[turma.dia][horarioAtual].push(turma.id);
-        } else {
-          if (!tabelaDeOcupacao[turma.dia]) {
-            tabelaDeOcupacao[turma.dia] = {};
-          }
-          tabelaDeOcupacao[turma.dia][horarioAtual] = [turma.id];
-        }
-      }
-    });
-    return tabelaDeOcupacao;
-  }
-
-  function getConflitos(tabelaDeOcupacao) {
-    let conflitos = [];
-    for (let dia in tabelaDeOcupacao) {
-      for (let horario in tabelaDeOcupacao[dia]) {
-        if (tabelaDeOcupacao[dia][horario].length > 1) {
-          conflitos.push({
-            dia: dia,
-            horario: horario,
-            turmas: tabelaDeOcupacao[dia][horario],
-          });
-        }
-      }
-    }
-    return conflitos;
+  function TabelaDemandasAtendidas() {
+    return <div></div>;
   }
 
   return (
     <div>
-      <div>Testing Conflicts</div>
+      <TabelaDemandasAtendidas />
     </div>
   );
 }
 
-export default ConflictsWorkbench;
+export default CalculoDemandasAtendidas;
