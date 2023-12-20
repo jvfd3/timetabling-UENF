b7bbfcc398086a64b4a209f1403e568864975962
jvfd3 <joaovitorfd2000@gmail.com>
2023-12-19 23:37:52 -0300

    changing order

--- Files/AWS/lambdas/professoresRead/index.mjs
+++ Files/AWS/lambdas/professoresRead/index.mjs
@@ -1,5 +1,4 @@
 // index.js
-// aws lambda create-function --function-name professoresRead --runtime nodejs20.x --role arn:aws:iam::375423677214:role/LambdaRole --handler index.handler --zip-file fileb://D:/HDExt/GitHub/UENF/9Semestre/timetabling-UENF/Files/AWS/lambdas/createProfessor/createProfessor.zip --environment "Variables={DB_HOST='dbtimetabling.cgsgwtemx5r8.us-east-2.rds.amazonaws.com',DB_USER='tang',DB_PASSWORD='annabell',DB_NAME='timetabling'}"
 import {createDbConnection} from "./db.js";
 let local = "aws>lambda>professores>Read";
 
@@ -34,8 +33,8 @@ function getPayloadResponse(message, query, queryValues, queryResult, error, sta
   let myBody = {
     message: message,
     query: query,
-    queryValues: queryValues,
     queryResult: queryResult,
+    queryValues: queryValues,
     error: error
   };
   let returnedMessage = {
