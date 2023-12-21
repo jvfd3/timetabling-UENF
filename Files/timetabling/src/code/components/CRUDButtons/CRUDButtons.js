dad84af91b1fa24fd1add35b7cbe2d3bc09c113c
jvfd3 <joaovitorfd2000@gmail.com>
2023-12-21 11:26:49 -0300

    Making CRUD Buttons sizeable

--- Files/timetabling/src/code/components/CRUDButtons/CRUDButtons.js
+++ Files/timetabling/src/code/components/CRUDButtons/CRUDButtons.js
@@ -2,34 +2,34 @@ import "./CRUDButtons.css";
 import { FaTrash, FaEdit } from "react-icons/fa";
 import { BsDatabaseDown, BsDatabaseFillAdd } from "react-icons/bs";
 
-function CreateButton({ receivedFunction }) {
+function CreateButton({ receivedFunction, size = "4em" }) {
   return (
     <div className="iconCreate">
-      <BsDatabaseFillAdd size="4em" onClick={receivedFunction} />
+      <BsDatabaseFillAdd size={size} onClick={receivedFunction} />
     </div>
   );
 }
 
-function ReadButton({ receivedFunction }) {
+function ReadButton({ receivedFunction, size = "4em" }) {
   return (
     <div className="iconRead">
-      <BsDatabaseDown size="4em" onClick={receivedFunction} />
+      <BsDatabaseDown size={size} onClick={receivedFunction} />
     </div>
   );
 }
 
-function UpdateButton({ receivedFunction }) {
+function UpdateButton({ receivedFunction, size = "4em" }) {
   return (
     <div className="iconUpdate">
-      <FaEdit size="4em" onClick={receivedFunction} />
+      <FaEdit size={size} onClick={receivedFunction} />
     </div>
   );
 }
 
-function DeleteButton({ receivedFunction }) {
+function DeleteButton({ receivedFunction, size = "4em" }) {
   return (
     <div className="iconDelete">
-      <FaTrash size="4em" onClick={receivedFunction} />
+      <FaTrash size={size} onClick={receivedFunction} />
     </div>
   );
 }
