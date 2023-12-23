1611283df5efb93776b149742663c577cda784bb
jvfd3 <joaovitorfd2000@gmail.com>
2023-12-22 22:23:50 -0300

    some simplification of my firulas' logic

--- Files/timetabling/src/code/functions/firulas/minhasFirulas.js
+++ Files/timetabling/src/code/functions/firulas/minhasFirulas.js
@@ -1,89 +1,72 @@
+function getIndexVariation(event) {
+  const UP = "up";
+  const DOWN = "down";
+  let diretion = event.deltaY > 0 ? DOWN : UP;
+  let indexVariation = diretion === UP ? -1 : 1;
+  return indexVariation;
+}
+
+function getNewIndex(currentIndex, itemsList, event) {
+  let indexVariation = getIndexVariation(event);
+  let fullIndex = currentIndex + indexVariation + itemsList.length;
+  let newIndex = fullIndex % itemsList.length;
+  return newIndex;
+}
+
 function scrollThroughProfessores(event, professorStates) {
   const { professores, setProfessor, professor } = professorStates;
-
-  let diretion = event.deltaY > 0 ? "down" : "up";
   let index = professores.findIndex(
     (oneOfProfessores) => oneOfProfessores.nome === professor.nome
   );
-  index += diretion === "up" ? -1 : 1;
-  index = index < 0 ? professores.length - 1 : index;
-  index = index >= professores.length ? 0 : index;
-  let newOption = professores[index];
+  let newOption = professores[getNewIndex(index, professores, event)];
   setProfessor(newOption);
 }
 
 function scrollThroughItemList(event, itemStates) {
   const [items, setItem, item] = itemStates;
-  const UP = "up";
-  const DOWN = "down";
-  let diretion = event.deltaY > 0 ? DOWN : UP;
   let index = items.findIndex((oneOfitems) => oneOfitems.nome === item.nome);
-  index += diretion === UP ? -1 : 1;
-  index = index < 0 ? items.length - 1 : index;
-  index = index >= items.length ? 0 : index;
-  let newOption = items[index];
+  let newOption = items[getNewIndex(index, items, event)];
   setItem(newOption);
 }
 function changePageByScrolling(event, itemStates) {
   const [filteredOptions, props, handleChange] = itemStates;
-  const UP = "up";
-  const DOWN = "down";
-  let diretion = event.deltaY > 0 ? DOWN : UP;
   let index = filteredOptions.findIndex(
     (option) => option.value === props.defaultValue.value
   );
-  index += diretion === UP ? -1 : 1;
-  index = (index + filteredOptions.length) % filteredOptions.length;
-  let newOption = filteredOptions[index];
+  let newOption = filteredOptions[getNewIndex(index, filteredOptions, event)];
   handleChange(newOption);
 }
 function scrollThroughDisciplinas(event, itemStates) {
-  let diretion = event.deltaY > 0 ? "down" : "up";
   const [disciplinas, setDisciplina, disciplina] = itemStates;
   let index = disciplinas.findIndex(
     (oneOfDisciplinas) => oneOfDisciplinas.codigo === disciplina.codigo
   );
-  index += diretion === "up" ? -1 : 1;
-  index = index < 0 ? disciplinas.length - 1 : index;
-  index = index >= disciplinas.length ? 0 : index;
-  let newOption = disciplinas[index];
+  let newOption = disciplinas[getNewIndex(index, disciplinas, event)];
   setDisciplina(newOption);
 }
 
 function scrollThroughSalas(event, itemStates) {
   const [salasFromJson, setSala, sala] = itemStates;
-  let diretion = event.deltaY > 0 ? "down" : "up";
   let index = salasFromJson.findIndex(
     (oneOfSalas) => oneOfSalas.blocoSala === sala.blocoSala
   );
-  index += diretion === "up" ? -1 : 1;
-  index = index < 0 ? salasFromJson.length - 1 : index;
-  index = index >= salasFromJson.length ? 0 : index;
-  let newOption = salasFromJson[index];
+  let newOption = salasFromJson[getNewIndex(index, salasFromJson, event)];
   setSala(newOption);
 }
 
 function scrollThroughTurmas(event, itemStates) {
   const [turmas, setTurma, turma] = itemStates;
-  let diretion = event.deltaY > 0 ? "down" : "up";
   let index = turmas.findIndex((oneOfTurmas) => oneOfTurmas.id === turma.id);
-  index += diretion === "up" ? -1 : 1;
-  index = index < 0 ? turmas.length - 1 : index;
-  index = index >= turmas.length ? 0 : index;
-  let newOption = turmas[index];
+  let newOption = turmas[getNewIndex(index, turmas, event)];
   setTurma(newOption);
 }
 
 function scrollThroughAlunos(event, itemsStates) {
   const [dados_agrupados, setAluno, aluno] = itemsStates;
-  let diretion = event.deltaY > 0 ? "down" : "up";
   let index = dados_agrupados.findIndex(
     (oneOfAlunos) => oneOfAlunos.matricula === aluno.matricula
   );
-  index += diretion === "up" ? -1 : 1;
-  index = index < 0 ? dados_agrupados.length - 1 : index;
-  index = index >= dados_agrupados.length ? 0 : index;
-  let newOption = dados_agrupados[index];
+  let newOption = dados_agrupados[getNewIndex(index, dados_agrupados, event)];
   setAluno(newOption);
 }
 
