3febfe01fca57420d86a4d35dccb8942fb126ec0
jvfd3 <joaovitorfd2000@gmail.com>
2023-12-24 14:53:29 -0300

    removing still unused alunos component

--- Files/timetabling/src/app/timetabling-UENF/alunos/alunos.js
+++ Files/timetabling/src/app/timetabling-UENF/alunos/alunos.js
@@ -91,106 +91,10 @@ function Alunos() {
       );
     }
 
-    function InformacoesDisciplinasAluno() {
-      function SelectDisciplinas(props) {
-        const { myPlaceHolder, myOptions, current_student, update_student } =
-          props;
-        return (
-          <Select
-            className="manyDisciplinasMultiSelect"
-            placeholder={myPlaceHolder}
-            options={allLocalJsonData.static.infoDisciplinasCC}
-            value={myOptions}
-            onChange={(option) => {
-              let myStudent = { ...current_student };
-              myStudent[myPlaceHolder] = option;
-              update_student(myStudent);
-            }}
-            isMulti={true}
-            isClearable={false}
-            isSearchable={true}
-            getOptionValue={(option) => option.codigo}
-            getOptionLabel={(option) => option.nome}
-            formatOptionLabel={({ codigo, nome }) => {
-              let text = `${codigo}: ${nome}`;
-              return text;
-            }}
-          />
-        );
-      }
-
-      return (
-        <div className="showBasicDataCard">
-          <h3>Andamento do Aluno</h3>
-          <div>
-            <table>
-              <thead>
-                <tr>
-                  <th>Cursando</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <SelectDisciplinas
-                      myPlaceHolder="cursando"
-                      myOptions={student.cursando}
-                      current_student={student}
-                      update_student={change_student}
-                    />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-
-            <table>
-              <thead>
-                <tr>
-                  <th>Não Feitas</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <SelectDisciplinas
-                      myPlaceHolder="naofeitas"
-                      myOptions={student.naofeitas}
-                      current_student={student}
-                      update_student={change_student}
-                    />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-
-            <table>
-              <thead>
-                <tr>
-                  <th>Aprovadas</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <SelectDisciplinas
-                      myPlaceHolder="aprovadas"
-                      myOptions={student.aprovadas}
-                      current_student={student}
-                      update_student={change_student}
-                    />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      );
-    }
-
     return (
       <div className="infoCard">
         <InformacoesBaseDoAluno />
-        <InformacoesDisciplinasAluno />
+        {/* <InformacoesDisciplinasAluno /> */}
       </div>
     );
   }
