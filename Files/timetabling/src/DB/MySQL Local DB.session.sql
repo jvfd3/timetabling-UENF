de95e983b4d6691d5b755cdfcea0614a3da3bb33
jvfd3 <joaovitorfd2000@gmail.com>
2023-12-24 14:32:28 -0300

    my local SQL queries

---
+++ Files/timetabling/src/DB/MySQL Local DB.session.sql
@@ -0,0 +1,75 @@
+/*
+Tenho o seguinte banco de dados:
+
+- disciplinas
+  - id (INT, PK)
+  - periodo (INT)
+  - codigo (VARCHAR)
+  - apelido (VARCHAR)
+  - nome (VARCHAR)
+- professores
+  - id (INT, PK)
+  - laboratorio (VARCHAR)
+  - curso (VARCHAR)
+  - apelido (VARCHAR)
+  - nome (VARCHAR)
+- salas
+  - id (INT, PK)
+  - capacidade (INT)
+  - bloco (VARCHAR)
+  - codigo (VARCHAR)
+  - descricao (VARCHAR)
+- turmas
+  - id (INT, PK)
+  - ano (INT)
+  - semestre (INT)
+  - demandaEstimada (INT)
+  - idDisciplina (INT, FK)
+  - idProfessor (INT, FK)
+- horarios
+  - id (INT, PK)
+  - dia (VARCHAR)
+  - horaInicio (INT)
+  - duracao (INT)
+  - ordem (INT)
+  - idTurma (INT, FK)
+  - idSala (INT, FK)
+*/
+
+
+/* Selecionar dados de turma' */
+SELECT
+  t.ano AS ano,
+  t.semestre AS semestre,
+  t.demandaEstimada AS demandaEstimada,
+  p.nome AS NomeProfessor,
+  p.apelido AS ApelidoProfessor,
+  p.curso AS CursoProfessor,
+  p.laboratorio AS LaboratorioProfessor,
+  d.nome AS NomeDisciplina,
+  d.apelido AS ApelidoDisciplina,
+  d.codigo AS CodigoDisciplina,
+  d.periodo AS PeriodoDisciplina
+  FROM turmas AS t
+    JOIN professores AS p
+      ON t.idProfessor = p.id
+    JOIN disciplinas AS d
+      ON t.idDisciplina = d.id;
+
+/* Selecionar horários */
+SELECT
+    h.idTurma AS idTurma,
+    h.ordem AS ordem,
+    h.dia AS dia,
+    h.horaInicio AS horaInicio,
+    h.duracao AS duracao,
+    s.capacidade AS capacidade,
+    s.bloco AS bloco,
+    s.codigo AS codigoSala,
+    s.descricao AS descricao
+  FROM horarios AS h
+    JOIN salas AS s
+      ON h.idSala = s.id
+    JOIN turmas AS t
+      ON h.idTurma = t.id
+ORDER BY h.idTurma, h.ordem;
