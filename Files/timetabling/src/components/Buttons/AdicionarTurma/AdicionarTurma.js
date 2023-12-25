e1cbd1d7db65ec028b136e730c39b2b4c2a3d4a1
jvfd3 <joaovitorfd2000@gmail.com>
2023-12-25 17:59:38 -0300

    new Button

---
+++ Files/timetabling/src/components/Buttons/AdicionarTurma/AdicionarTurma.js
@@ -0,0 +1,9 @@
+function AdicionarTurma({ addTurma }) {
+  return (
+    <button className="AdicionarHorario" onClick={addTurma}>
+      Nova Turma
+    </button>
+  );
+}
+
+export default AdicionarTurma;
