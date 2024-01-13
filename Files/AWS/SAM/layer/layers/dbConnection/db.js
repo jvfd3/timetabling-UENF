1e24e17ccf02d358d5a8b82ae0fdd3b1c7e87660
jvfd3 <joaovitorfd2000@gmail.com>
2024-01-13 15:09:04 -0300

    #223 updating Lambda Layer on SAM

---
+++ Files/AWS/SAM/layer/layers/dbConnection/db.js
@@ -0,0 +1,34 @@
+const mysql = require("mysql2/promise");
+
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
+
+async function createDbConnection() {
+  try {
+    return await mysql.createConnection(dbConfig);
+  } catch (err) {
+    let error = new Error(["db.js>createDbConnection", err]);
+    console.error(error);
+    throw error;
+  }
+}
+
+async function dbExecute(query, values = null) {
+  try {
+    let dbConnection = await createDbConnection();
+    let queryResult = await dbConnection.execute(query, values);
+    await dbConnection.end();
+    return queryResult;
+  } catch (err) {
+    console.log("Edited?");
+    let error = new Error(["db.js>dbExecute", err]);
+    console.error(error);
+    throw error;
+  }
+}
+
+module.exports = { dbExecute };
