2f1a1ec8284543048b16fd734dbec051ec23c3dd
jvfd3 <joaovitorfd2000@gmail.com>
2024-01-25 22:18:07 -0300

    creating multiclasses table component

---
+++ Files/timetabling/src/components/ClassItemTable/ClassItemTable.js
@@ -0,0 +1,111 @@
+import "./ClassItemTable.css";
+import React, { useState, useEffect, useRef } from "react";
+import { baseClassItemConflicts } from "../../helpers/conflicts/centralConflicts";
+import { NumberInputMultiClassesExpectedDemand } from "../MyTextFields";
+import { SelectClassProfessor, SelectClassSubject } from "../mySelects";
+import ClassTimeTable from "../ClassTimeTable/ClassTimeTable";
+import {
+  createClass,
+  deleteClass,
+  updateClass,
+} from "../../helpers/CRUDFunctions/classCRUD";
+import {
+  SmartCreateClassItem,
+  SmartDeleteClassItem,
+  SmartUpdateClassItem,
+} from "../Buttons/Smart/Smart";
+import { getId } from "../../helpers/auxCRUD";
+
+function ClassTableHeader(createStates) {
+  return (
+    <thead>
+      <tr>
+        <th>
+          <SmartCreateClassItem {...createStates} />
+        </th>
+        <th>Código - Nome</th>
+        <th>Professor</th>
+        <th>Demanda Estimada</th>
+        <th colSpan={5}>Horários</th>
+      </tr>
+    </thead>
+  );
+}
+
+function ClassItemTableRow(classItemRowStates) {
+  const { classItem, filteredClasses } = classItemRowStates;
+
+  const [oldClassItem, setOldClassItem] = useState(classItem);
+
+  const conflicts = baseClassItemConflicts(
+    filteredClasses,
+    classItem,
+    classItem?.semestre
+  );
+
+  const classTimeTableProps = {
+    ...classItemRowStates,
+    conflicts,
+  };
+
+  const CRUDClassItemProps = {
+    classItem,
+    oldClassItem,
+    deleteClassItemDB: () => deleteClass(classItemRowStates),
+    updateClassItemDB: () => updateClass(classItemRowStates),
+  };
+
+  const classItemRowKey = `ClassItemTableRow: ${classItem?.idTurma}-${classItem?.disciplina?.id}-${classItem?.professor?.id}`;
+
+  return (
+    <tr key={classItemRowKey}>
+      <td>
+        <SmartDeleteClassItem {...CRUDClassItemProps} />
+        <SmartUpdateClassItem {...CRUDClassItemProps} />
+      </td>
+      <td {...conflicts.styled.disciplina}>
+        <SelectClassSubject {...classItemRowStates} />
+      </td>
+      <td {...conflicts.styled.professor}>
+        <SelectClassProfessor {...classItemRowStates} />
+      </td>
+      <td {...conflicts.styled.demand}>
+        <NumberInputMultiClassesExpectedDemand {...classItemRowStates} />
+      </td>
+      <td>
+        <ClassTimeTable {...classTimeTableProps} />
+      </td>
+    </tr>
+  );
+}
+
+function ClassesTable(classStates) {
+  const { filteredClasses } = classStates;
+
+  const createStates = {
+    classesStates: classStates,
+    year: classStates.classItem.ano,
+    semester: classStates.classItem.semestre,
+    createClassDB: createClass,
+  };
+
+  return (
+    <table className="showBasicDataTable">
+      <ClassTableHeader {...createStates} />
+      <tbody>
+        {filteredClasses.map((iterClassItem) => {
+          const classItemRowProps = {
+            ...classStates,
+            classItem: iterClassItem,
+          };
+          const classItemRowKey = `ClassItemTableRow: ${getId(iterClassItem)}`;
+          return (
+            <ClassItemTableRow {...classItemRowProps} key={classItemRowKey} />
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export default ClassesTable;
