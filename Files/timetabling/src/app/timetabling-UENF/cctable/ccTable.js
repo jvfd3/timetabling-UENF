164a997586f8febd1ef50eb98e49bf3160ecefa9
jvfd3 <joaovitorfd2000@gmail.com>
2024-01-29 15:35:05 -0300

    #264 removing unused components

--- Files/timetabling/src/app/timetabling-UENF/cctable/ccTable.js
+++ Files/timetabling/src/app/timetabling-UENF/cctable/ccTable.js
@@ -2,124 +2,13 @@ import "./ccTable.css";
 import React, { useEffect, useState } from "react";
 import options from "../../../DB/local/options";
 import CRUDPageSelection from "../../../components/PageSelect";
-import { getClassesData } from "../../../DB/retrieveData";
-import { splitTurmas } from "../../../helpers/conflicts/auxConflictFunctions";
-import { filterDay, filterHour } from "../../../helpers/filteringFunc";
-import {
-  CCTableFiltersOffline,
-  CCTableFilters,
-} from "../../../components/Filters/Filters";
+import CCTableDB from "./ccTableDB";
+import { CCTableFilters } from "../../../components/Filters/Filters";
 import { getDefaultClassTime } from "../../../helpers/auxCRUD";
 import { readClassTime } from "../../../helpers/CRUDFunctions/classTimeCRUD";
 import { readSubject } from "../../../helpers/CRUDFunctions/subjectCRUD";
 import { readProfessor } from "../../../helpers/CRUDFunctions/professorCRUD";
 import { readRoom } from "../../../helpers/CRUDFunctions/roomCRUD";
-import CCTableDB from "./ccTableDB";
-
-function getCellMessage(turma) {
-  // console.log("turma", turma.sala);
-  const subject = turma.disciplina;
-  const prof = turma.professor;
-  const room = turma.sala;
-  const subjectInfo = subject
-    ? `${subject?.periodo} - ${subject?.apelido}`
-    : "Discip. indef.";
-  const profInfo = prof ? `${prof.apelido}` : "Prof. indef.";
-  const roomInfo = room
-    ? `${room?.bloco}${room?.codigo ? "-" + room?.codigo : ""}`
-    : "Sala indef.";
-  const cellMessage = `${subjectInfo} (${profInfo} / ${roomInfo})`;
-  return cellMessage;
-}
-
-function CellContent({ turmas }) {
-  const listaDeTurmas = turmas.map((turma) => {
-    const cellMessage = getCellMessage(turma);
-    const cellKey = `ChaveCellContent: ${turma.idTurma}-${turma.idHorario}`;
-    return (
-      <div key={cellKey} className="eachClassInCell">
-        {cellMessage}
-      </div>
-    );
-  });
-
-  return listaDeTurmas;
-}
-
-function Linha({ hora, curClasses }) {
-  const turmasDaHora = filterHour(curClasses, hora);
-
-  const daysColumn = options.constantValues.days.map((dia) => {
-    const turmasDoDia = filterDay(turmasDaHora, dia.value);
-
-    return (
-      <td key={`Key Coluna: ${dia.value}-${hora}`} className="ContentCell">
-        <CellContent turmas={turmasDoDia} />
-      </td>
-    );
-  });
-
-  return (
-    <tr key={`Linha: ${hora}`}>
-      <td className="HorariosCol" key={`Linha: ${hora}, Header: ${hora}`}>
-        {hora}
-      </td>
-      {daysColumn}
-    </tr>
-  );
-}
-
-function TopRow() {
-  const daysList = options.constantValues.days;
-  const days = daysList.map((day, index) => {
-    return (
-      <th key={index} className="DiasHeader">
-        {day.label}
-      </th>
-    );
-  });
-
-  return [days];
-}
-
-function TopLeft() {
-  return <th className="TopLeftCorner"></th>;
-}
-
-function Header() {
-  return (
-    <thead>
-      <tr className="HeaderRow">
-        <TopLeft />
-        <TopRow />
-      </tr>
-    </thead>
-  );
-}
-
-function Body({ curClasses }) {
-  const hoursTangList = options.constantValues.hoursTang;
-  return (
-    <tbody>
-      {hoursTangList.map((iterHour) => (
-        <Linha
-          key={`Linha: ${iterHour.hora}`}
-          hora={iterHour.hora}
-          curClasses={curClasses}
-        />
-      ))}
-    </tbody>
-  );
-}
-
-function CCTableOffline({ curClasses }) {
-  return (
-    <table className="TabelaCC">
-      <Header />
-      <Body curClasses={curClasses} />
-    </table>
-  );
-}
 
 function CCTableView() {
   const [classTimes, setClassTimes] = useState([]);
@@ -166,21 +55,11 @@ function CCTableView() {
     readClassTime(classTimeStates);
   }, []);
 
-  const turmas = getClassesData();
-  const allSplittedClasses = splitTurmas(turmas);
-  const [currentClasses, setCurrentClasses] = useState(allSplittedClasses);
-
-  const filterStates = { allSplittedClasses, setCurrentClasses };
-
   return (
     <div className="CRUDContainComponents">
       <div className="infoCard">
         <CCTableFilters {...globalStates} />
         <CCTableDB classTimes={filteredClassTimes} />
-        {/*
-        <CCTableFiltersOffline {...filterStates} />
-        <CCTableOffline curClasses={currentClasses} />
-        */}
       </div>
     </div>
   );
