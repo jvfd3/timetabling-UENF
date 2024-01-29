a03ee2642ec9a876e369c44b84fabd16085cfd3b
jvfd3 <joaovitorfd2000@gmail.com>
2024-01-29 17:33:50 -0300

    #264 removing unused components

--- Files/timetabling/src/components/Filters/Filters.js
+++ Files/timetabling/src/components/Filters/Filters.js
@@ -125,47 +125,6 @@ function MultiClassesFilters({ classTimeStates, classStates }) {
   );
 }
 
-function CCTableFiltersOffline({ setCurrentClasses, allSplittedClasses }) {
-  const years = options.constantValues.years;
-  const yearIndex = options.config.defaultIndexes.year;
-  const semesters = options.constantValues.semesters;
-  const semesterIndex = options.config.defaultIndexes.semester;
-
-  const [year, setYear] = useState(years[yearIndex]);
-  const [semester, setSemester] = useState(semesters[semesterIndex]);
-  const [professor, setProfessor] = useState(null);
-  const [room, setRoom] = useState(null);
-  const [expectedSemester, setExpectedSemester] = useState(null);
-
-  const props = {
-    year: { year, setYear },
-    semester: { semester, setSemester },
-    professor: { professor, setProfessor },
-    room: { room, setRoom },
-    expectedSemester: { expectedSemester, setExpectedSemester },
-  };
-
-  useEffect(() => {
-    let filtering = allSplittedClasses;
-    filtering = filterYear(filtering, year);
-    filtering = filterSemester(filtering, semester);
-    filtering = filterProfessor(filtering, professor);
-    filtering = filterRoom(filtering, room);
-    filtering = filterExpectedSemester(filtering, expectedSemester);
-    setCurrentClasses(filtering);
-  }, [year, semester, professor, room, expectedSemester]);
-
-  return (
-    <div className="CCTableFilters">
-      <FilterYear {...props.year} />
-      <FilterSemester {...props.semester} />
-      <FilterProfessor {...props.professor} />
-      <FilterRoom {...props.room} />
-      <FilterExpectedSemester {...props.expectedSemester} />
-    </div>
-  );
-}
-
 function CCTableFilters(globalStates) {
   const { classTimeStates, selectStates } = globalStates;
   const { classTimes, setFilteredClassTimes, setClassTime } = classTimeStates;
@@ -211,4 +170,4 @@ function CCTableFilters(globalStates) {
   );
 }
 
-export { MultiClassesFilters, CCTableFiltersOffline, CCTableFilters };
+export { MultiClassesFilters, CCTableFilters };
