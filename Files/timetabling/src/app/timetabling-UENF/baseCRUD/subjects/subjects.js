d7e67dcaa10de14d3f909a860e399172c991fadc
jvfd3 <joaovitorfd2000@gmail.com>
2024-02-03 12:05:03 -0300

    #274 Removing the comment from Subjects page

--- Files/timetabling/src/app/timetabling-UENF/baseCRUD/subjects/subjects.js
+++ Files/timetabling/src/app/timetabling-UENF/baseCRUD/subjects/subjects.js
@@ -143,23 +143,3 @@ function CRUDSubjects() {
 }
 
 export default CRUDSubjects;
-
-/* O que esta página deve fazer?
-
-- Fazer uma visualização bonitinha para os selects:
-  - Select de Código: Disciplina deve estar em cima na esquerda
-  - Select de Período: à esquerda do Select de código
-  - Select de professores deve estar abaixo do Select de Códigos.
-  - Select de requisitos deve estar abaixo do Select de professores.
-- [x] Devo obter as informações das disciplinas do JSONBIN
-- Transformar as informações para que cada nome de disciplina esteja na key "label" e o código na key "value"
-- devo converter a lista de codigos de requisitos para uma lista de dicionários com a estrutura {label: "nome", value: "codigo"}
-  - essa lista deve ser definida como value do Select de requisitos
-- a seleção de disciplinas é a seleção principal, seu valor base deve ser a disciplina monografia
-- ao alterar a disciplina, deve-se alterar todos os outros selects.
-- ao alterar cada um dos selects, deve-se alterar o valor do estado da página.
-- para cada disciplina, deve-se vasculhar quais são os professores que o têm na lista de disciplinas que ministram.
-  - Os professores encontrados devem se tornar uma lista de dicionários com a estrutura {value = "nome do professor", label = "laboratório"}
-  - esse valor deve estar definido como value no Select de Docentes
-- ao alterar cada um dos selects, a alteração deve ser enviada ao JSONBIN. - DO LATER
-*/
