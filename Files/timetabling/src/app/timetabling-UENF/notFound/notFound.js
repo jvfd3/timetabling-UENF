b73333b8d944a41c5ecaa037acb0b2a5ae41510a
jvfd3 <joaovitorfd2000@gmail.com>
2024-02-03 21:18:15 -0300

    #326 #284 Adding text for NotFound page

--- Files/timetabling/src/app/timetabling-UENF/notFound/notFound.js
+++ Files/timetabling/src/app/timetabling-UENF/notFound/notFound.js
@@ -5,14 +5,19 @@ import myStyles from "../../../config/myStyles";
 
 const defaultClassNames = myStyles.classNames.default;
 
+const texts = {
+  title: "Is this a 404 page?",
+  message: "I guess not. 💠",
+};
+
 function notFound() {
   const defaultPageValue = configInfo.pageSelection.notFound;
   return (
     <div className={defaultClassNames.background}>
       <CRUDPageSelection defaultValue={defaultPageValue} />
-        <h1>Is this a 404 page?</h1>
-        <p>I guess not. 💠</p>
       <div className={defaultClassNames.containerCards}>
+        <h1>{texts.title}</h1>
+        <p>{texts.message}</p>
       </div>
     </div>
   );
