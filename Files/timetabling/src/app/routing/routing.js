03b641df02dd943d56a16a54a87cfbad84b11afd
jvfd3 <joaovitorfd2000@gmail.com>
2024-02-05 14:23:56 -0300

    #272 Sorting by length

--- Files/timetabling/src/app/routing/routing.js
+++ Files/timetabling/src/app/routing/routing.js
@@ -8,29 +8,30 @@ const { routing, pageSelection } = configInfo;
 // import PageSelection from "./components/PageSelection/PageSelect";
 // Page imports
 /* Isso poderia ser trocado pro valor do options */
+
+import myStyles from "../../config/myStyles";
 import Main from "../timetabling-UENF/main/main";
 import Classes from "../timetabling-UENF/classes/classes";
-import MultiClasses from "../timetabling-UENF/multiClasses/MultiClasses";
-import NoMatch from "../timetabling-UENF//notFound/notFound";
 import CCTable from "../timetabling-UENF//cctable/ccTable";
-import Professors from "../timetabling-UENF//baseCRUD/professors/professors";
+import Rooms from "../timetabling-UENF/baseCRUD/rooms/rooms";
+import NoMatch from "../timetabling-UENF//notFound/notFound";
 import Students from "../timetabling-UENF/baseCRUD/students/students";
 import Subjects from "../timetabling-UENF/baseCRUD/subjects/subjects";
-import Rooms from "../timetabling-UENF/baseCRUD/rooms/rooms";
+import MultiClasses from "../timetabling-UENF/multiClasses/MultiClasses";
 import PageSelection from "../../components/PageSelection/PageSelection";
-import myStyles from "../../config/myStyles";
+import Professors from "../timetabling-UENF//baseCRUD/professors/professors";
 
 function MyRouting() {
   const basePath = routing.urlPath; //"/timetabling-uenf/";
   const pathMain = basePath + pageSelection.main.url;
-  const pathMultiClasses = basePath + pageSelection.multiClasses.url;
+  const pathRooms = basePath + pageSelection.rooms.url;
   const pathClasses = basePath + pageSelection.classes.url;
+  const pathCCtable = basePath + pageSelection.CCTable.url;
+  const pathSubjects = basePath + pageSelection.subjects.url;
+  const pathNotFound = basePath + pageSelection.notFound.url;
   const pathStudents = basePath + pageSelection.students.url;
   const pathProfessors = basePath + pageSelection.professors.url;
-  const pathSubjects = basePath + pageSelection.subjects.url;
-  const pathRooms = basePath + pageSelection.rooms.url;
-  const pathNotFound = basePath + pageSelection.notFound.url;
-  const pathCCtable = basePath + pageSelection.CCTable.url;
+  const pathMultiClasses = basePath + pageSelection.multiClasses.url;
 
   const defaultClassNames = myStyles.classNames.default;
 
@@ -40,18 +41,18 @@ function MyRouting() {
         <PageSelection />
         <Routes>
           <Route element={<Main />} index />
-          <Route element={<Main />} path={"/"} />
+          <Route element={<Main />} path="/" />
+          <Route element={<NoMatch />} path="*" />
           <Route element={<Main />} path={basePath} />
           <Route element={<Main />} path={pathMain} />
-          <Route element={<MultiClasses />} path={pathMultiClasses} />
+          <Route element={<Rooms />} path={pathRooms} />
           <Route element={<CCTable />} path={pathCCtable} />
           <Route element={<Classes />} path={pathClasses} />
-          <Route element={<Professors />} path={pathProfessors} />
-          <Route element={<Rooms />} path={pathRooms} />
+          <Route element={<NoMatch />} path={pathNotFound} />
           <Route element={<Subjects />} path={pathSubjects} />
           <Route element={<Students />} path={pathStudents} />
-          <Route element={<NoMatch />} path={pathNotFound} />
-          <Route element={<NoMatch />} path="*" />
+          <Route element={<Professors />} path={pathProfessors} />
+          <Route element={<MultiClasses />} path={pathMultiClasses} />
         </Routes>
         <ToastContainer {...configInfo.toast} />
       </BrowserRouter>
--- Files/timetabling/src/index.js
+++ Files/timetabling/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import MyRouting from "./app/routing/routing";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <MyRouting />
