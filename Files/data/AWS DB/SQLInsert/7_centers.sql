3bb05317d2919b29c65c01d3b400f4b13050fe0f
jvfd3 <joaovitorfd2000@gmail.com>
2024-02-18 19:30:01 -0300

    #207 Updating sql files

---
+++ Files/data/AWS DB/SQLInsert/7_centers.sql
@@ -0,0 +1,5 @@
+INSERT INTO centers (`id`, `alias`, `name`) VALUES
+  (1, 'CCT', 'Centro de Ciência e Tecnologia'),
+  (2, 'CBB', 'Centro de Biociencias e Biotecnologia'),
+  (3, 'CCTA', 'Centro de Ciências e Tecnologias Agropecuárias'),
+  (4, 'CCH', 'Centro de Ciências do Homem');
