9ba9b32e7fcdc2a4d66e52be0f96164d9af0cec7
jvfd3 <joaovitorfd2000@gmail.com>
2024-02-24 00:12:11 -0300

    #272 Indexes

--- Files/timetabling/src/config/configInfo.js
+++ Files/timetabling/src/config/configInfo.js
@@ -58,8 +58,8 @@ const configInfo = {
       "https://mckwruc3w1.execute-api.us-east-2.amazonaws.com/timetablingStage",
   },
   defaultIndexes: {
-    year: 6, // 10: 2024
-    semester: 1, // 1
+    year: 5, // 10: 2024
+    semester: 0, // 1
     professor: 2, // Tang
     student: 38, // João Vítor Fernandes Dias
     subject: 58, // Monografia
