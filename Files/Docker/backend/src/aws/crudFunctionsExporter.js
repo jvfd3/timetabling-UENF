bfeaaeb87b8d6e3e4954e56a9ea2093e6802fc6a
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2025-07-12 02:54:35 -0300

    Sorting

--- Files/Docker/backend/src/aws/crudFunctionsExporter.js
+++ Files/Docker/backend/src/aws/crudFunctionsExporter.js
@@ -1,30 +1,31 @@
 import { createClassData } from "./classData/createClassData.js";
 import { createClassTime } from "./classTime/createClassTime.js";
 import { createProfessor } from "./professor/createProfessor.js";
-import { createRoom } from "./room/createRoom.js";
 import { createStudent } from "./student/createStudent.js";
 import { createSubject } from "./subject/createSubject.js";
+import { createRoom } from "./room/createRoom.js";
 
 import { readClassData } from "./classData/readClassData.js";
 import { readClassTime } from "./classTime/readClassTime.js";
 import { readProfessor } from "./professor/readProfessor.js";
-import { readRoom } from "./room/readRoom.js";
 import { readStudent } from "./student/readStudent.js";
 import { readSubject } from "./subject/readSubject.js";
+import { readRoom } from "./room/readRoom.js";
 
 import { updateClassData } from "./classData/updateClassData.js";
 import { updateClassTime } from "./classTime/updateClassTime.js";
 import { updateProfessor } from "./professor/updateProfessor.js";
-import { updateRoom } from "./room/updateRoom.js";
 import { updateStudent } from "./student/updateStudent.js";
 import { updateSubject } from "./subject/updateSubject.js";
+import { updateRoom } from "./room/updateRoom.js";
 
 import { deleteClassData } from "./classData/deleteClassData.js";
 import { deleteClassTime } from "./classTime/deleteClassTime.js";
 import { deleteProfessor } from "./professor/deleteProfessor.js";
-import { deleteRoom } from "./room/deleteRoom.js";
 import { deleteStudent } from "./student/deleteStudent.js";
 import { deleteSubject } from "./subject/deleteSubject.js";
+import { deleteRoom } from "./room/deleteRoom.js";
+
 export {
   createClassData,
   createClassTime,
